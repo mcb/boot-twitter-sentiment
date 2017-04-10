6b90a16f9afce2e8c39821ae005e128122c18255
Jan Svoboda <spasitel2@gmail.com>
2017-04-10 11:26:39 +0200

    changed output target to copy

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -6,5 +6,6 @@ echo $version
 cd bts-repo
 mvn package
 
-cp bts-repo/boot-twitter-feeder/target/* binaries/
-cp bts-repo/boot-twitter-visualizer/target/* binaries/
+pwd
+cp boot-twitter-feeder/target/* binaries/
+cp boot-twitter-visualizer/target/* binaries/
