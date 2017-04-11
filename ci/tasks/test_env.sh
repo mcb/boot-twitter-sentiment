9c4b2fe8448ce1d792840058089eda583060f24f
jan svoboda <spasitel2@gmail.com>
2017-04-11 10:20:24 +0200

    Update test_env.sh

--- ci/tasks/test_env.sh
+++ ci/tasks/test_env.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+curl -I $TEST_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2
 get_code="curl -I $TEST_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
-status_code=eval $get_code
+status_code=`eval $get_code`
+
 if [ "$status_code" != "200" ]
 then
   echo "Expect status code from $TEST_URL as 200, but got $status_code"
-  exit 1
 fi
